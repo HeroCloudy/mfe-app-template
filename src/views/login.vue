4db4b5ec04abc3954e65d2683d89ca385c1b3bce
liuyun <yunliuly@cn.ibm.com>
2024-05-02 14:17:49 +0800

    添加登录路由

---
+++ src/views/login.vue
@@ -0,0 +1,18 @@
+<!--
+ @name: login.vue
+ @description: 登录页面
+ @author: 程序员优雅哥 yygcoder
+ @time: 2024/4/22 20:19
+-->
+<template>
+  <div class="login">login</div>
+</template>
+
+<script setup lang="ts">
+console.log('login')
+</script>
+
+<style scoped lang="scss">
+.login {
+}
+</style>
